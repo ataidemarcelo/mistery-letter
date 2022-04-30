f448f04c6c92f52e1aebb77710ff102978fc6b95
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-30 09:30:20 -0300

    Requisito 4 - Ao criar uma carta através do botão com id="criar-carta", o `input` com id="carta-texto" deve permanecer com o texto digitado

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Project Mistery Letter!!!');
+
+const btnCreateLetter = document.getElementById('criar-carta');
+const generatedLetter = document.getElementById('carta-gerada');
+
+function createLetter(event) {
+  event.preventDefault();
+  generatedLetter.innerText = '';
+
+  const inputLetterText = document.getElementById('carta-texto');
+  const textWords = inputLetterText.value.split(' ');
+
+  for (let index = 0; index < textWords.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = `${textWords[index]} `;
+    generatedLetter.appendChild(span);
+  }
+}
+
+btnCreateLetter.addEventListener('click', createLetter);

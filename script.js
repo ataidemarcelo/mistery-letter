07cedeaaf0b5ebc412b89c4fad36592edd0318a6
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-30 12:04:22 -0300

    Requisito 16 - Adicione as classes de forma aleatória a fim de estilizar as palavras.

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ console.log('Project Mistery Letter!!!');
 const btnCreateLetter = document.getElementById('criar-carta');
 const generatedLetter = document.getElementById('carta-gerada');
 
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const skew = ['skewleft', 'skewright'];
+
+const arrayListOfClasses = [style, size, rotate, skew];
+
+function addClasses(item, arrayList) {
+  const element = item;
+  for (let index = 0; index < arrayList.length; index += 1) {
+    const num = Math.floor(Math.random() * arrayList[index].length);
+    element.classList.add(arrayList[index][num]);
+  }
+}
+
 function createLetter(event) {
   event.preventDefault();
   generatedLetter.innerText = '';
 
   const inputLetterText = document.getElementById('carta-texto');
   const textWords = inputLetterText.value.split(' ');
-
   const textLength = inputLetterText.value.trim().length;
 
   if (textLength === 0) {
@@ -20,6 +34,7 @@ function createLetter(event) {
   for (let index = 0; index < textWords.length; index += 1) {
     const span = document.createElement('span');
     span.innerText = `${textWords[index]}`;
+    addClasses(span, arrayListOfClasses);
     generatedLetter.appendChild(span);
   }
 }
